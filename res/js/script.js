bb3be6146c214590d2065decea503f455555c506
ZxAnugrah <anugrahsukmono@gmail.com>
2026-06-23 11:30:28 +0700

    maintenance 24/06/2026

--- res/js/script.js
+++ res/js/script.js
@@ -80,7 +80,7 @@ function setupCountdown(liId, endDateStr) {
 document.addEventListener("DOMContentLoaded", function () {
   setupCountdown(5601, "2026-01-31T00:00:00"); // index_chn.html (unused)
 
-  setupCountdown(6000, "2026-06-10T14:00:00"); // maintenance.htm
+  setupCountdown(6000, "2026-06-24T12:00:00"); // maintenance.htm
   setupCountdown(6001, "2026-05-13T15:30:00"); // temp-maintenance.htm
 
   setupCountdown(6002, "2026-01-31T23:59:59"); // news.htm
